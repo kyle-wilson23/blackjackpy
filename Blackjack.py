2738884259165fb88fe7ae475fb76215742c40df
Kyle Wilson <kyle.j.wilson@outlook.com>
2020-05-07 21:08:38 -0400

    test commit from kyle

--- Blackjack.py
+++ Blackjack.py
@@ -1,6 +1,8 @@
 from random import randint
 import time
 
+# test comment from kyle
+
 deck = []
 suits = ['diamonds', 'hearts', 'spades', 'clubs']
 nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
